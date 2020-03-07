3be31e9b198f126f5de777f7fef5402fdae118f1
Kevin <guzman@MacBook-Pro-9.lan>
2020-03-06 20:24:35 -0500

    Committing project files

---
+++ app/src/main/java/com/example/totalrec/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.totalrec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "Firebase Connection Succcessful", Toast.LENGTH_LONG ).show();
+    }
+
+
+}
